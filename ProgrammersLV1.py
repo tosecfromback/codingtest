ed603bfd5e84c408c35af1def2e9640ccd2e196e
tosecfromback <131732872+tosecfromback@users.noreply.github.com>
2023-05-08 01:38:42 +0900

    Update ProgrammersLV1.py
    from 23.05.07 but it's past the date

--- ProgrammersLV1.py
+++ ProgrammersLV1.py
@@ -57,12 +57,12 @@ def solution(numbers):
         answer.append(numbers[i]*2)
     return answer
 
-#10th  https://school.programmers.co.kr/learn/courses/30/lessons/120810
+#09th  https://school.programmers.co.kr/learn/courses/30/lessons/120810
 def solution(num1, num2):
     answer = num1 % num2
     return answer
 
-#11th  https://school.programmers.co.kr/learn/courses/30/lessons/120811
+#10th  https://school.programmers.co.kr/learn/courses/30/lessons/120811
 def solution(array):
     for i in range(len(array)):
         for x in range(i+1, len(array)):
@@ -72,11 +72,61 @@ def solution(array):
     answer = array[y]
     return answer
 
+#11th  https://school.programmers.co.kr/learn/courses/30/lessons/120812
+def solution(array):
+    most_num = 0
+    most_count = 0
+    if len(array) > 1:
+        for i in range(len(array)):
+            if array[i] != most_num:
+                if most_count < array.count(array[i]):
+                    most_count = array.count(array[i])
+                    most_num = array[i]
+                    answer = most_num
+                elif most_count == array.count(array[i]):
+                    answer = -1
+    elif len(array) == 1:
+        answer = array[0]
+    return answer
+
+#12th  https://school.programmers.co.kr/learn/courses/30/lessons/120813
+def solution(n):
+    answer = []
+    for i in range(0, n+1, 1):
+        if i % 2 == 1:
+            answer.append(i)
+    return answer
 
 
-#08th  https://school.programmers.co.kr/learn/courses/30/lessons/120809
-#08th  https://school.programmers.co.kr/learn/courses/30/lessons/120809
-#08th  https://school.programmers.co.kr/learn/courses/30/lessons/120809
-#08th  https://school.programmers.co.kr/learn/courses/30/lessons/120809
-#08th  https://school.programmers.co.kr/learn/courses/30/lessons/120809
-#08th  https://school.programmers.co.kr/learn/courses/30/lessons/120809
+#13th  https://school.programmers.co.kr/learn/courses/30/lessons/120814
+def solution(n):
+    if n <= 7:
+        answer = 1
+    elif n > 7 and (n % 7) == 0:
+        answer = (n // 7)
+    elif n > 7 and (n % 7) != 0:
+        answer = (n // 7) + 1
+    return answer
+
+#14th  https://school.programmers.co.kr/learn/courses/30/lessons/120815
+def solution(n):
+    for i in range(1, (n * 6)+1):
+        if i % n == 0 and i % 6 == 0:
+            answer = (i // 6)
+            break
+    return answer
+
+#15th  https://school.programmers.co.kr/learn/courses/30/lessons/120816
+def solution(slice, n):
+    for i in range(1, (slice*n)+1):
+        if slice*i >= n:
+            answer = i
+            break
+    return answer
+#16th  https://school.programmers.co.kr/learn/courses/30/lessons/120817
+def solution(numbers):
+    answer = 0
+    for i in range(len(numbers)):
+        answer = answer + numbers[i]
+    answer = answer / len(numbers)
+    return answer
