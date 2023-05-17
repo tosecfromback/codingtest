83ff6ad64dae482ee66610199f439899316789be
tosecfromback <131732872+tosecfromback@users.noreply.github.com>
2023-05-17 18:07:30 +0900

    Update ProgrammersLV0.py

--- ProgrammersLV0.py
+++ ProgrammersLV0.py
@@ -322,16 +322,78 @@ def solution(my_string, n):
 
 #39th https://school.programmers.co.kr/learn/courses/30/lessons/120835
 
-
 #40th https://school.programmers.co.kr/learn/courses/30/lessons/120834
+def solution(age):
+    answer = ''
+    age_table = { '0' : 'a' ,'1' : 'b', '2' : 'c', '3' : 'd', '4' : 'e', '5' : 'f', '6' : 'g', '7' : 'h', '8' : 'i', '9' : 'j'}
+    for i in str(age):
+        answer = answer + age_table[i]
+    return answer
 
 #41th https://school.programmers.co.kr/learn/courses/30/lessons/120837
+def solution(hp):
+    answer = 0
+    ant = [5, 3, 1]
+    for i in ant:
+        j = 0
+        if i >= ( hp % i ) >= 0:
+            answer += hp // i
+            j = hp//i
+            hp -= j*i
+    return answer
 
 #42th https://school.programmers.co.kr/learn/courses/30/lessons/120838
+morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+
+def solution(letter):
+    letter+= ' '
+    answer = ''
+    decoded = ''
+    for i in letter:
+        if i != ' ':
+            decoded+=i
+        else:
+            answer += morse[decoded]
+            decoded = ''
+    return answer
 
 #43th https://school.programmers.co.kr/learn/courses/30/lessons/120839
+# over Python3.11+
+def solution(rsp):
+    answer = ''
+    for i in rsp:
+        match int(i):
+            case 0:
+                answer = answer+'5'
+            case 2:
+                answer = answer+'0'
+            case 5:
+                answer = answer+'2'
+    return answer
+# under Python3.11
+def solution(rsp):
+    answer = ''
+    for i in rsp:
+        if i == '0':
+            answer = answer+'5'
+        elif i == '2':
+            answer = answer+'0'
+        elif i == '5':
+            answer = answer+'2'
+    return answer
 
 #44th https://school.programmers.co.kr/learn/courses/30/lessons/120840
+import math
+
+def solution(balls, share):
+    answer = math.factorial(balls) // ((math.factorial(balls - share) * math.factorial(share)))    
+    return answer
 
 #45th https://school.programmers.co.kr/learn/courses/30/lessons/120956
 import re
